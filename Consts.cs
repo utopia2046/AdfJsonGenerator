39dd114ccdd642932774625a93df46ec0a274d45
utopia2046 <utopia2046@hotmail.com>
2016-02-23 15:12:52 +0800

    Add files via upload

---
+++ Consts.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace AdfJsonGenerator
+{
+    class Consts
+    {
+        public static string LinkedServiceNameTemplate = @"AzureStorage_{0}";   // AzureStorage_eastusbingads
+        public static string LinkedServiceJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/LinkedServiceJsonTemplate.json");
+            }
+        }
+
+        public static string InputTableStorageNameTemplate = @"InputTable_{0}_{1}";  // InputTable_WU_Trace
+        public static string InputTableStorageJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/InputTableStorageJsonTemplate.json");
+            }
+        }
+
+        public static string OutputBlobStorageNameTemplate = @"OutputBlob_{0}_{1}";  // OutputBlob_WU_Trace
+        public static string OutputBlobStorageJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/OutputBlobStorageJsonTemplate.json");
+            }
+        }
+
+        public static string PipelineNameTemplate = "Table2Blob_{0}_{1}";   // Table2Blob_WU_Trace
+        public static string PipelineJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/PipelineJsonTemplate.json");
+            }
+        }
+
+        public static string InputAzureBlobSiNameTemplate = "InputAzureBlob_siwestus_<tableName>";
+        public static string InputAzureBlobSiJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/InputAzureBlobSiTemplate.json");
+            }
+        }
+
+        public static string OutputCosmosSiNameTemplate = "OutputCosmos_siwestus_<tableName>";
+        public static string OutputCosmosSiJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/OutputCosmosSiTemplate.json");
+            }
+        }
+
+        public static string Azure2CosmosPipelineSiNameTemplate = "Azure2Cosmos_siwest_<tableName>";
+        public static string Azure2CosmosPipelineSiJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/Azure2CosmosPipelineSiTemplate.json");
+            }
+        }
+
+        public static List<TableInfo> _tables = null;
+        public static List<TableInfo> Tables
+        {
+            get
+            {
+                if (_tables == null)
+                {
+                    _tables = new List<TableInfo>();
+                    _tables.Add(new TableInfo("Error", "error", "Error", "error"));
+                    _tables.Add(new TableInfo("ApplicationError", "apperror", "Error", "error"));
+                    _tables.Add(new TableInfo("UserError", "usererror", "Error", "error"));
+                    _tables.Add(new TableInfo("Performance", "perf", "Perf", "perf"));
+                    _tables.Add(new TableInfo("Latency", "latency", "Perf", "perf"));
+                    _tables.Add(new TableInfo("Trace", "trace", "Trace", "trace"));
+                }
+
+                return _tables;
+            }
+        }
+
+        private static List<StorageAccount> _storageAccounts = null;
+        public static List<StorageAccount> StorageAccounts
+        {
+            get
+            {
+                if (_storageAccounts == null)
+                {
+                    _storageAccounts = new List<StorageAccount>();
+                    _storageAccounts.Add(new StorageAccount(0, "EU", "eastusbingads", "1d8zTqBuglJLuWml8L/DpoHQ0Lvv7mA9hH5gQ7vJ7Wy5DTdrbpMLCC4Df8A+qmubUZudn+VRGeUqCFeelvze3g=="));
+                    _storageAccounts.Add(new StorageAccount(1, "WU", "westusbingads", "OTcq85y56fJ6bKITzvI0qC9MaUOcgKZ5WEMK25ao55R3B0tzOkaT8gjA8P1qj+KSOFzjiQPBaVFLiPDz0L+BBA=="));
+                    _storageAccounts.Add(new StorageAccount(2, "NE", "northeuropebingads", "E2/XTSkMnnyySbDOBvwodIv/Rw+6ggkTaUFPiXua3PIl8dfnIbsKuilYbLQ1MUnpoSvRfwsA9WCPe05U/FrWQQ=="));
+                    _storageAccounts.Add(new StorageAccount(3, "SEA", "southeastasiabingads", "JpnPlIGSz/hw86L51NFR6b+3EMMAcQI3LC76PP1YWU+EJ6obQuyeiECI8lHQWcdNMqb/wMFckci/Ed/NOAWgjw=="));
+                    _storageAccounts.Add(new StorageAccount(4, "NCU", "northcentralusbingads", "xGtvfZ7CvmMjdk3EBloC/uAP7vranmlT5iq7+1QZCMxmLSnkIkpBU7ZbwsJ2iRhtDdnMovMXXvVLlOxtrgZVDA=="));
+                }
+
+                return _storageAccounts;
+            }
+        }
+
+        public static string InputBlobNameTemplate = @"InputAzureBlob_Prod_<tableName>";
+        public static string InputBlobJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/InputAzureBlobTemplate.json");
+            }
+        }
+
+        public static string OutputCosmosNameTemplate = @"OutputCosmos_Prod_<tableName>";
+        public static string OutputCosmosJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/OutputCosmosTemplate.json");
+            }
+        }
+
+        public static string Azure2CosmosPipelineNameTemplate = @"Azure2Cosmos_Prod_<tableName>";
+        public static string Azure2CosmosPipelineJsonTemplate
+        {
+            get
+            {
+                return ReadTemplate("JsonTemplates/Azure2CosmosPipelineTemplate.json");
+            }
+        }
+
+        private static string ReadEmbeddedResource(string name)
+        {
+            string content;
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(name))
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+
+            return content;
+        }
+
+        private static string ReadTemplate(string name)
+        {
+            string content;
+            using (var sr = new StreamReader(name))
+            {
+                content = sr.ReadToEnd();
+            }
+
+            return content;
+        }
+    }
+
+    public class StorageAccount
+    {
+        public int Index;
+        public string ShortRegion;
+        public string AccountName;
+        public string Key;
+
+        public StorageAccount(int index, string shortRegion, string accountName, string key)
+        {
+            Index = index;
+            ShortRegion = shortRegion;
+            AccountName = accountName;
+            Key = key;
+        }
+    }
+
+    public class TableInfo
+    {
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public string CosmosFolderName { get; set; }
+        public string Extension { get; set; }
+
+        public TableInfo(string name, string fileName, string cosmosFolderName, string extension)
+        {
+            Name = name;
+            FileName = fileName;
+            CosmosFolderName = cosmosFolderName;
+            Extension = extension;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdfJsonGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateAzureTable2BlobJsons();
+            GenerateAzureBlob2CosmosJsons();
+            GenerateSiAzure2CosmosJsons();
+        }
+
+        static void GenerateSiAzure2CosmosJsons()
+        {
+            foreach (var table in Consts.Tables)
+            {
+                // Generate Input Blob storages json file
+                var blobDataSetName = Consts.InputAzureBlobSiNameTemplate.Replace("<tableName>", table.Name);
+                var blobDataSetJson = Consts.InputAzureBlobSiJsonTemplate.Replace("<tableName>", table.Name);
+                WriteJsonFile(blobDataSetName, blobDataSetJson);
+
+                // Generate Output Cosmos storage json file
+                var cosmosDataSetName = Consts.OutputCosmosSiNameTemplate.Replace("<tableName>", table.Name);
+                var cosmosDataSetJson = Consts.OutputCosmosSiJsonTemplate.Replace("<tableName>", table.Name);
+                WriteJsonFile(cosmosDataSetName, cosmosDataSetJson);
+
+                // Generate Pipeline json file
+                var pipelineName = Consts.Azure2CosmosPipelineSiNameTemplate.Replace("<tableName>", table.Name);
+                var pipelineJson = Consts.Azure2CosmosPipelineSiJsonTemplate.Replace("<tableName>", table.Name);
+                WriteJsonFile(pipelineName, pipelineJson);
+            }
+        }
+
+        static void GenerateAzureBlob2CosmosJsons()
+        {
+            foreach (var table in Consts.Tables)
+            {
+                // Generate Input Blob storages json file
+                var blobDataSetName = Consts.InputBlobNameTemplate.Replace("<tableName>", table.Name);
+                var blobDataSetJson = Consts.InputBlobJsonTemplate.Replace("<tableName>", table.Name);
+                WriteJsonFile(blobDataSetName, blobDataSetJson);
+
+                // Generate Output Cosmos storage json file
+                var cosmosDataSetName = Consts.OutputCosmosNameTemplate.Replace("<tableName>", table.Name);
+                var cosmosDataSetJson = Consts.OutputCosmosJsonTemplate.Replace("<tableName>", table.Name)
+                    .Replace("<cosmosFolder>", table.CosmosFolderName);
+                WriteJsonFile(cosmosDataSetName, cosmosDataSetJson);
+
+                // Generate Pipeline json file
+                var pipelineName = Consts.Azure2CosmosPipelineNameTemplate.Replace("<tableName>", table.Name);
+                var pipelineJson = Consts.Azure2CosmosPipelineJsonTemplate.Replace("<tableName>", table.Name);
+                WriteJsonFile(pipelineName, pipelineJson);
+            }
+        }
+
+        static void GenerateAzureTable2BlobJsons()
+        {
+            foreach (var account in Consts.StorageAccounts)
+            {
+                var shortRegion = account.ShortRegion;
+                var accountName = account.AccountName;
+                var key = account.Key;
+                var index = account.Index;
+
+                // Generate linked service
+                var linkedServiceName = String.Format(Consts.LinkedServiceNameTemplate, accountName);
+                var LinkedServiceJson = Consts.LinkedServiceJsonTemplate.Replace("<accountName>", accountName)
+                    .Replace("<description>", accountName)
+                    .Replace("<key>", key);
+                WriteJsonFile(linkedServiceName, LinkedServiceJson);
+
+                foreach (var table in Consts.Tables)
+                {
+                    // Generate Input Table storages json file
+                    var tableDataSetName = String.Format(Consts.InputTableStorageNameTemplate, shortRegion, table.Name);
+                    var tableDataSetJson = Consts.InputTableStorageJsonTemplate.Replace("<regionShort>", shortRegion)
+                        .Replace("<tableName>", table.Name)
+                        .Replace("<accountName>", accountName);
+                    WriteJsonFile(tableDataSetName, tableDataSetJson);
+
+                    // Generate Output Blob storage json file
+                    var blobDataSetName = String.Format(Consts.OutputBlobStorageNameTemplate, shortRegion, table.Name);
+                    var blobDataSetJson = Consts.OutputBlobStorageJsonTemplate.Replace("<regionShort>", shortRegion)
+                        .Replace("<tableName>", table.Name)
+                        .Replace("<tableNameShort>", table.FileName)
+                        .Replace("<accountName>", accountName)
+                        .Replace("<index>", index.ToString())
+                        .Replace("<extension>", table.Extension);
+                    WriteJsonFile(blobDataSetName, blobDataSetJson);
+
+                    // Generate Pipeline json file
+                    var pipelineName = String.Format(Consts.PipelineNameTemplate, shortRegion, table.Name);
+                    var pipelineJson = Consts.PipelineJsonTemplate.Replace("<regionShort>", shortRegion)
+                        .Replace("<tableName>", table.Name)
+                        .Replace("<accountName>", accountName);
+                    WriteJsonFile(pipelineName, pipelineJson);
+                }
+            }
+        }
+
+        static void WriteJsonFile(string name, string content)
+        {
+            using (var sw = new StreamWriter(name + ".json"))
+            {
+                sw.Write(content);
+                Console.WriteLine("JSON file {0}.json written successfully", name);
+            }
+        }
+
+        static bool IsFileExist(string name)
+        {
+            if (File.Exists(name + ".json"))
+            {
+                Console.WriteLine("Target file exist.");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
